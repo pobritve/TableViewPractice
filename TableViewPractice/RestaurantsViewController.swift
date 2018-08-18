c71c4cd25640d8f995710326ab2c6dc0595cffdd
Viktor Shaban <pobritve@mail.ru>
2018-08-18 21:22:14 +0300

    Initial commit

---
+++ TableViewPractice/RestaurantsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RestaurantsViewController.swift
+//  TableViewPractice
+//
+//  Created by Viktor Shaban on 18.08.2018.
+//  Copyright © 2018 no name. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
